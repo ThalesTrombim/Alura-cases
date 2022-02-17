263bcc1dc6c9667b24325d829ab0625ce99b0b8a
ThalesTrombim <thalestr@outlook.com>
2022-02-17 19:54:24 -0300

    final da aula 3

---
+++ pages/_app.jsx
@@ -0,0 +1,11 @@
+import GlobalStyle from '../src/components/theme/GlobalStyle'
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    )
+  }
+  
+export default MyApp
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,36 @@
-import { NextLink } from '../src/components/Link';
+import HomeScreen from '../src/screens/HomeScreen';
 
-export default function HomePage() {
-    return (
-        <div>
-            <h1>Alura cases</h1>
-            <NextLink href='/faq'>
-                ir para o faq
-            </NextLink>
-        </div>
-    )
-}
+export default HomeScreen;
+
+// import { NextLink } from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>
+//                 { children }
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+                    
+//                 }
+//             `}</style>
+//         </>
+//     )
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as='h1'>
+//                 Alura cases
+//             </Title>
+//             <NextLink href='/faq'>
+//                 ir para o faq
+//             </NextLink>
+//             <Footer />
+//         </div>
+//     )
+// }
